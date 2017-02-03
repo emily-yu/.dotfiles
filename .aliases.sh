d6f97af2bf87908d423573b381498ca8c281af6e
ey1031 <eyudeveloper@gmail.com>
2017-02-02 19:48:57 -0800

    works

--- .aliases.sh
+++ .aliases.sh
@@ -27,20 +27,28 @@ function noapi() {
 	#don't commit all the -e-e-e-e-e-e-ee-e-e files
 }
 
+function getvariable() {
+}
+
 #commits a version to local git with <YOUR-API-KEY>
 function apiremove() { #$1 takes first parameter, $2 takes second parameter, etc. 
 	#retrieve variable of apiKey
 	read apiKey < $1
 	IFS=’=’ 
 	read -ra apiKey <<< "$apiKey" 
-	NO_WHITESPACE="$(echo -e "${apiKey[1]}" | tr -d '[:space:]')"
-	echo -e "${NO_WHITESPACE}" #echo value of apiKey variable
+	NO_WHITESPACE="$(echo "${apiKey[1]}" | tr -d '[:space:]')"
+	echo "${NO_WHITESPACE}" #echo value of apiKey variable
 
 	#replace api, commit it, and replace it with original api variable again
 	temp=${NO_WHITESPACE}  #sets temp to original api key variable 
 	echo $temp
-	sed -i -e "s/$temp/'<YOUR-API-KEY>'/g" $1 #changes value of first to second
-	git add $1 #git add w/ replaced key - UNTESTED
-    git commit -m $2 #git add with message
-    sed -i -e "s/'<YOUR-API-KEY>'/$temp/g" $1 #put the api key variable back in local repo
+	sed -i -e"s/$temp/'<YOUR-API-KEY>'/g" $1 #changes value of first to second
+	# git add $1 #git add w/ replaced key - UNTESTED
+ #    git commit -m $2 #git add with message
+ #    sed -i -e"s/'<YOUR-API-KEY>'/$temp/g" $1 #put the api key variable back in local repo
 }
+
+function remove-e() {
+	ls -d '*-e*' | egrep -v '^e|e$' | xargs rm
+}
+
